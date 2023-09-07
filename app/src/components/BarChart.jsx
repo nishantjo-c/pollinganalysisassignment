faf765eb19b594248802ff7ae5079ccad75bae00
nishantjo-c <nishantjoshiofficial@gmail.com>
2023-09-07 14:49:53 +0530

    final touch

--- app/src/components/BarChart.jsx
+++ app/src/components/BarChart.jsx
@@ -1,6 +1,34 @@
 import { Bar } from "react-chartjs-2";
-import { Chart as ChartJS } from "chart.js/auto";
+import { Chart as ChartJS, plugins } from "chart.js/auto";
 
 export default function BarChart({ chartData }) {
-  return <Bar data={chartData} />;
+  const option = {
+    plugins: {
+      legend: {
+        labels: {
+          color: "white",
+        },
+      },
+    },
+    scales: {
+      y: {
+        ticks: {
+          color: "white",
+          font: {
+            size: 13,
+          },
+        },
+      },
+      x: {
+        ticks: {
+          color: ["red", "green"],
+          font: {
+            size: 13,
+          },
+        },
+      },
+    },
+  };
+
+  return <Bar data={chartData} options={option} />;
 }
--- app/src/components/LineChart.jsx
+++ app/src/components/LineChart.jsx
@@ -2,9 +2,36 @@ import { Line } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 
 export default function LineChart({ chartData }) {
+  const option = {
+    plugins: {
+      legend: {
+        labels: {
+          color: "white",
+        },
+      },
+    },
+    scales: {
+      y: {
+        ticks: {
+          color: "white",
+          font: {
+            size: 13,
+          },
+        },
+      },
+      x: {
+        ticks: {
+          color: "black",
+          font: {
+            size: 13,
+          },
+        },
+      },
+    },
+  };
   return (
     <>
-      <Line data={chartData} />
+      <Line data={chartData} options={option} />
     </>
   );
 }
