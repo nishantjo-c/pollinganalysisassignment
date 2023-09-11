9652520ea84c527124132ba15dd04597c0ed9a40
nishantjo-c <nishantjoshiofficial@gmail.com>
2023-09-11 22:39:33 +0530

    vercel

--- server/database/db.js
+++ server/database/db.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 
 const Pool = require("pg").Pool;
+// const pool = new Pool({
+//   user: "postgres",
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   host: "localhost",
+//   port: 5432,
+// });
+
 const pool = new Pool({
-  user: "postgres",
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: "localhost",
-  port: 5432,
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
 module.exports = pool;
